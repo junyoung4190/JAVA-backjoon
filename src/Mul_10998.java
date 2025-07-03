64bc18d364c4bac330f69084a588d1ac05f4de08
JYoung <yeoundu4190@naver.com>
2025-07-03 13:25:59 +0900

    두 정수 A와 B를 입력받은 다음, AXB를 출력하는 프로그램을 작성하시오

---
+++ src/Mul_10998.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Mul_10998 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();;
+        int B = scanner.nextInt();;
+
+        int res = A * B;
+
+        System.out.println(res);
+    }
+}
