34b27ac86817deed662358e3da9cf48c87874019
JYoung <yeoundu4190@naver.com>
2025-07-03 13:16:40 +0900

    두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오

---
+++ src/덧셈_1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class 덧셈_1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int sum = A + B;
+
+        System.out.println(sum);
+    }
+}
