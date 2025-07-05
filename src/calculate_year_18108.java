f9f595df9f5739982ba51b47200c26bced81d4e5
JYoung <yeoundu4190@naver.com>
2025-07-05 17:05:17 +0900

    불교연도를 서기연도로변환

---
+++ src/calculate_year_18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class calculate_year_18108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int B_year = scanner.nextInt();
+
+        System.out.println(B_year - 543);
+    }
+}
