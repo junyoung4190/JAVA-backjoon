dc89827a6c835f705219659942ab20e98c9756c0
JYoung <yeoundu4190@naver.com>
2025-07-05 17:49:34 +0900

    CAT

---
+++ src/Cat_10171.java
@@ -0,0 +1,10 @@
+public class Cat_10171 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
