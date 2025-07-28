0141ce1fff9e1bac4d1e7eec4021d2c2edc66154
JYoung <yeoundu4190@naver.com>
2025-07-28 21:47:16 +0900

    BR을 이용한 반복문 덧셈

---
+++ src/반복문/Sum_CaseIII_v2_10952.java
@@ -0,0 +1,28 @@
+package 반복문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Sum_CaseIII_v2_10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while(true){
+            st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if(A == 0 && B == 0){
+                break;
+            }
+
+            sb.append((A + B)).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
